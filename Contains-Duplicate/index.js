9ad1ae3e506c979ec4bd81c9a0adf7269ab1acdd
ismailbenlaredj <yako_is@yahoo.fr>
2023-04-05 23:01:26 +0100

    add Contains Duplicate

---
+++ Contains-Duplicate/index.js
@@ -0,0 +1,18 @@
+const arr1 = [1, 2, 3, 1, 3, 4, 5, 6, 2] //true
+const arr2 = [1, 2, 3, 4] //false
+
+const containsDuplicate = (arr) => {
+    let ob = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (ob[arr[i]] !== undefined) {
+            return true
+        } else {
+            ob[arr[i]] = arr[i]
+        }
+    }
+    return false
+}
+
+
+console.log(containsDuplicate(arr1))
+console.log(containsDuplicate(arr2))
